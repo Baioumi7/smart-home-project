f9e8bc1ddd03de18201b6c8ed4ead165214e78d4
Ahmed Bayoumii <125226127+Baioumi7@users.noreply.github.com>
2023-11-17 20:50:13 +0200

    Add files via upload

---
+++ code/Master code/Master code/Master code/LCD_config.h
@@ -0,0 +1,15 @@
+/*
+ * LCD_config.h
+ *
+* Created: 2/9/2022 4:46:19 PM
+*  Author: ahmed bayoumy
+ */ 
+
+
+#ifndef LCD_CONFIG_H_
+#define LCD_CONFIG_H_
+
+#define eight_bits_mode
+//#define four_bits_mode
+
+#endif /* LCD_CONFIG_H_ */
